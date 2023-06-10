c327ca73e4b52ab55c9be7cfa28084456aa715b8
Mankianer <mankianer@gmail.com>
2023-06-11 01:19:47 +0200

    add simple naps2.console scanner

---
+++ naps2.console/scan_doc.sh
@@ -0,0 +1,6 @@
+
+# TODO: Add a check if the scanner is available
+# TODO: Use TEMP folder for pdfs
+naps2.console -o "G:\Meine Ablage\Dokumente\Scans\test_\$(nnn)_\$(YY)-\$(MM)-\$(DD).pdf" -p "CANON P-208II" --ocrlang "ger" --enableocr
+
+# Scan Inhalt der PDF und Sortiere in Kategorie ein
