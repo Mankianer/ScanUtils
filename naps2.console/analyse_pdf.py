8591f7e65e32d33868c050f95304b2664a485858
Mankianer <mankianer@gmail.com>
2023-06-11 15:31:27 +0200

    add Simple ChatGPT analyse

--- naps2.console/analyse_pdf.py
+++ naps2.console/analyse_pdf.py
@@ -5,6 +5,9 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 import os
 import openai
+import collections
+from typing import List, Dict
+from fuzzywuzzy import fuzz
 
 # load openai api key
 openaiKeyPath = os.path.expanduser('~/.scanner_utils/openai')
@@ -44,19 +47,66 @@ def extract_text_from_pdf(pdf_path):
     return None
 
 
+def find_most_frequent_files(directory: str, num_files: int = 10) -> str:
+    results = {}
+
+    # Durchläuft jedes Verzeichnis und jede Datei im gegebenen Verzeichnis
+    for root, dirs, files in os.walk(directory):
+        # Prüft, ob die Datei 'kategorie' existiert
+        if 'kategorie' in files:
+            # Entfernt die 'kategorie' Datei aus der Dateiliste
+            files.remove('kategorie')
+            if len(files) > num_files:
+                remove_similar(files, num_files)
+
+            # Fügt die Ergebnisse zu den Gesamtergebnissen hinzu
+            category = os.path.relpath(root, directory)
+            results[category] = ['"' + file + '"' for file in files]
+
+    # Formatiert die Ergebnisse als String
+    result_string = "Kategorie: \"Datei1\",\"Datei2\",\"Datei3\"\n"
+    for category, files in results.items():
+        result_string += f"{category}: {', '.join(files)}\n"
+
+    return result_string
+
+
+def remove_similar(files, num_files):
+    # Bewertet die Ähnlichkeit jedes Dateipaares
+    similarity_scores = {}
+    for file1 in files:
+        for file2 in files:
+            if file1 != file2:
+                similarity_scores[(file1, file2)] = fuzz.ratio(file1, file2)
+    # Entfernt die ähnlichsten Dateien, bis nur noch die gewünschte Anzahl von Dateien übrig ist
+    while len(files) > num_files:
+        # Findet das Dateipaar mit der höchsten Ähnlichkeit
+        most_similar_pair = max(similarity_scores, key=similarity_scores.get)
+        # Findet die Datei mit der höchsten Gesamtähnlichkeit
+        most_similar_file = max(most_similar_pair, key=lambda file: sum(
+            score for (file1, file2), score in similarity_scores.items() if file1 == file or file2 == file))
+        # Entfernt die ähnlichste Datei
+        files.remove(most_similar_file)
+        # Aktualisiert die Ähnlichkeitsbewertungen
+        similarity_scores = {(file1, file2): score for (file1, file2), score in similarity_scores.items() if
+                             file1 != most_similar_file and file2 != most_similar_file}
+
+
 if __name__ == '__main__':
-    textout = extract_text_from_pdf('testpdf.pdf')
-    print("Textausgabe:")
-    print(textout)
-    # Liste aller Modelle abrufen
-    # TODO: Auflistung der Vorhandenen PDFs basierend auf Kategorie
-    system_promt = """
+    kategories_prompt = find_most_frequent_files('G:/Meine Ablage/Dokumente')
+    pdf_text = extract_text_from_pdf('testpdf.pdf')
+    # erstelle prompt
+    system_prompt = f"""
     Wähle ein passenden Titel mit Zeitangabe, um die PDF in einem Dateisystem zu ordnen zu können und Ordne der PDF eine der Kategorien zu!
-    Kategorien: Arbeit,Versicherung,Schule,Compute,Nicht-Zuordenbar.
-    Antworte mit: Titel: "<Titel>", Kategorie: "<Kategorie>"
+    {kategories_prompt}
+    Antworte im Json-Format mit den Eigenschaften Titel und Kategorie."
     """
+    print("Prompt:")
+    print(system_prompt)
+
+    # sende prompt an openai
     chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                   messages=[{"role": "system", "content": system_promt},
-                                                             {"role": "user", "content": f"PDF:{textout}"}])
+                                                   messages=[{"role": "system", "content": system_prompt},
+                                                             {"role": "user", "content": f"PDF:{pdf_text}"}])
     print("Chatausgabe:")
     print(chat_completion.choices[0].message.content)
